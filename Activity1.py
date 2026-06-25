40de1e7f35470835be9f453c54fb8dd8661dbf17
Amitt <neeta79.ns@gmail.com>
2026-06-25 13:32:04 +0530

    first commit

---
+++ Activity1.py
@@ -0,0 +1,26 @@
+
+print("Hello, world!")
+
+print("My name is Amitt.")
+
+print("I am 11 years old")
+
+print("I live in India.")
+
+print("I love playing cricket.")
+
+print("The result of 3 + 5 is:", 3 + 5)
+
+
+print("Welcome to the world of programming!")
+
+
+print(4)
+
+
+print("Hello Python \n")
+
+
+print("Keywords in Python are....")
+import keyword
+print(keyword.kwlist)
