54583e349a85d6c98e4477db4139094d92d4a9b7
Amitt <neeta79.ns@gmail.com>
2026-09-08 20:43:05 +0530

    Activity2

---
+++ Lesson 22 Playing with Numbers/Activity2.py
@@ -0,0 +1,8 @@
+numberLargest=int(input("Enter Largest number :"))
+numberSmallest=int(input("Enter Smallset number :"))
+
+while(numberSmallest):
+    numberStore = numberSmallest
+    numberSmallest=numberLargest % numberSmallest
+    numberLargest=numberStore
+print("HCF is :",numberLargest)
