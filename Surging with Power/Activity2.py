220f01cc845007d419ef9dc8069413522a4261dd
Amitt <neeta79.ns@gmail.com>
2026-09-24 20:50:15 +0530

    Activity2

---
+++ Surging with Power/Activity2.py
@@ -0,0 +1,11 @@
+input("power of 4: n % 3 == 1 Power of 8: n % 7 == 1. Press ENTER")
+print("16 binary:", bin(16)[2:],"16 % 3 =", 16 & 3,"Press power of 4: yes")
+print("8 binary:", bin(8)[2:],"8 % 7 =", 8 & 7,"Press power of 8: yes")
+
+n=int(input("Enter a number(try 64 or 32):"))
+guess=input("Is "+ str(n) +" a power of 4? (yes/no)")
+input("Check: n % 3 == 1. press ENTER")
+is_pow4 = n>0 and (n & (n-1))==0 and n % 3 == 1
+if is_pow4:
+     print("",n,"binary:",bin(n)[2:],"power of 4: yes your guess:",guess)
+     print("",n,"binary:",bin(n)[2:],"power of 4: yes your guess:",guess)
